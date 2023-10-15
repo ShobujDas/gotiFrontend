60029f4a51e833e1de3cbf98e3295023839135b2
Rafiul <mrafiul.alam7@gmail.com>
2023-10-15 18:52:20 +0600

    solved authFolers error

--- src/component/pages/auth/Signin.jsx
+++ src/component/pages/auth/Signin.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import Layout from '../../layout/Layout'
-import from './SignInStyle.css'
+import React from "react";
+import Layout from "../../layout/Layout";
+import "./SignInStyle.css";
 
 function Signin() {
   return (
     <Layout>
-        <div class="login-box">
+      <main>
+      <div className="login-box">
         <h2>Login</h2>
         <form>
-          <div class="user-box">
-            <input type="text" name="" required="">
+          <div className="user-box">
+            <input type="text" name required />
             <label>Username</label>
           </div>
-          <div class="user-box">
-            <input type="password" name="" required="">
+          <div className="user-box">
+            <input type="password" name required />
             <label>Password</label>
           </div>
           <a href="#">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
+            <span />
+            <span />
+            <span />
+            <span />
             Submit
           </a>
         </form>
       </div>
+      </main>
+      
     </Layout>
-  )
+  );
 }
 
-export default Signin
+export default Signin;
